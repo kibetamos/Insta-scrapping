57f7ec90ba9bb975ad9797018af5c548fabc3129
kibet kilel <amos.rotich@strathmore.edu>
2021-11-06 23:34:07 +0300

    models added

--- instascrap/models.py
+++ instascrap/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class Instagram(models.Model):
+    instagramuser = models.CharField(max_length=1000)
+    imagelink = models.CharField(max_length=10000)
+    username = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.instagramuser
