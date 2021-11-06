d2d50ce9e07ca53d0b8e1ad9e02b87a476e7d932
kibet kilel <amos.rotich@strathmore.edu>
2021-11-06 22:53:48 +0300

    update

--- instascrap/urls.py
+++ instascrap/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
     # path('register', views.register, name='register'),
     # path('login', views.login, name='login'),
     # path('logout', views.logout, name='logout'),
-    # path('images', views.images, name='images')
-
+    path('images', views.images, name='images')
 ]
--- instascrap/views.py
+++ instascrap/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
 def index(request):
+    if request.method == 'POST':
+        github_user = request.POST['github_user']
+        user = request.POST['user']
+        url = 'https://www.instagram.com/' + github_user
+        r = requests.get(url)
+        soup = bs(r.content)
+        profile = soup.find('img', {'alt': 'Avatar'})['src']
+
+        new_github = Github(
+            githubuser=github_user,
+            imagelink=profile,
+            username=user
+        )
+        new_github.save()
+        messages.info(request, 'User ' + github_user + ' Image Saved')
+        return redirect('/')
+
     return render(request, 'index.html')
+def images(request):
+    username = request.user
+    git_hub = Github.objects.filter(username=username)
+
+    return render(request, 'images.html', {'git_hub': git_hub})
